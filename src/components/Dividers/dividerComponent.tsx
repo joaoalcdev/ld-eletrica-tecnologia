baa000b6cdb7c385a5216bf8d8393b5ecce1a944
João Alcântara <joaosantalcantara@gmail.com>
2023-08-17 12:36:13 -0300

    Style: Updated old styles sections -> link sections styles

--- src/components/Dividers/dividerComponent.tsx
+++ src/components/Dividers/dividerComponent.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const DividerComponent = () => {
   return (
     <>
-      <hr className="my-16 sm:my-16 h-[1.5px] border-t-0 bg-transparent bg-gradient-to-r from-transparent via-blue-500 to-transparent opacity-25 dark:opacity-100" />
+      <hr className="h-1 border-t-0 bg-transparent bg-gradient-to-r from-transparent via-blue-500 to-transparent opacity-25 dark:opacity-100" />
     </>
   );
 };
