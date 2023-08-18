ca27ee28cdb251c95325e1bccfa40a79a19276fa
João Alcântara <joaosantalcantara@gmail.com>
2023-08-18 14:29:56 -0300

    Update content Text page

--- src/components/AboutSection/aboutSection.tsx
+++ src/components/AboutSection/aboutSection.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+import Image from "next/image";
 
 const AboutSection = () => {
   return (
@@ -9,26 +10,31 @@ const AboutSection = () => {
           <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
             <div className="font-light text-black sm:text-lg ">
               <h2 className="mb-4 text-2xl tracking-tight font-extrabold text-black">
-                Nós somos uma empresa...
+                LD Elétrica e Tecnologia
               </h2>
               <p className="mb-4">
-                We are strategists, designers and developers. Innovators and
-                problem solvers.
+                Somos especializados em fornecer serviços de elétrica e
+                tecnologia de alta qualidade que atendem às suas necessidades.
               </p>
               <p>
-                We are strategists, designers and developers. Innovators and
-                problem solvers. Small enough to be simple and quick.
+                Com nossos profissionais qualificados e anos de experiência,
+                estamos comprometidos em oferecer excelência em cada projeto que
+                realizamos.
               </p>
             </div>
             <div className="grid grid-cols-2 gap-4 mt-8">
-              <img
+              <Image
                 className="w-full rounded-lg"
                 src="/images/background_image.jpg"
+                width={50}
+                height={50}
                 alt="office content 1"
               />
-              <img
+              <Image
                 className="mt-4 w-full lg:mt-10 rounded-lg"
                 src="/images/background_image.jpg"
+                width={50}
+                height={50}
                 alt="office content 2"
               />
             </div>
