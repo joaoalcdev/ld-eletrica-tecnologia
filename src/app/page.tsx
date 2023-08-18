307c84475528228805e1fa373c4175da834cb499
João Alcântara <joaosantalcantara@gmail.com>
2023-08-18 12:48:57 -0300

    Feat: Added new Sectin to Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import AboutSection from "@/components/AboutSection/aboutSection";
 import DividerComponent from "@/components/Dividers/dividerComponent";
 import TestimonialsSection from "@/components/Testimonials/testimonialsSection";
 import Footer from "@/components/Footer/footer";
+import CTASection from "@/components/CTASection/ctaSection";
 
 export default function Home() {
   return (
@@ -15,6 +16,8 @@ export default function Home() {
       <AboutSection></AboutSection>
       <DividerComponent></DividerComponent>
       <TestimonialsSection></TestimonialsSection>
+      <DividerComponent></DividerComponent>
+      <CTASection></CTASection>
       <DividerComponent></DividerComponent>
       <Footer></Footer>
     </>
