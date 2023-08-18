21ed6988ceac6a56422281a7532c223bc544773d
João Alcântara <joaosantalcantara@gmail.com>
2023-08-18 14:17:46 -0300

    Style: Update CTA Button & Text

--- src/components/HeroSection/heroSection.tsx
+++ src/components/HeroSection/heroSection.tsx
@@ -46,17 +46,10 @@ const HeroSection = () => {
               </p>
               <div className="flex items-center">
                 <a
-                  href="/"
-                  className="inline-flex items-center justify-center h-12 px-4 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-blue-400 hover:bg-blue-700 focus:shadow-outline focus:outline-none"
+                  href="https://wa.me/5588993368262?text=Ol%C3%A1%21+Vi+seu+n%C3%BAmero+pelo+site.+Gostaria+de+mais+informa%C3%A7%C3%B5es+sobre+os+servi%C3%A7os."
+                  className="inline-flex items-center justify-center h-12 px-4 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-blue-700 hover:bg-blue-800 focus:shadow-outline focus:outline-none"
                 >
-                  Lorem Ipsum
-                </a>
-                <a
-                  href="/"
-                  aria-label=""
-                  className="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-blue-700"
-                >
-                  LOREM IPSU
+                  Entrar em contato
                 </a>
               </div>
             </div>
