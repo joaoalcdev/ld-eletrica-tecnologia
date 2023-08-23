9441f33c8852856f5e06a55269314b70ce0ba0ac
João Alcântara <joaosantalcantara@gmail.com>
2023-08-23 17:30:06 -0300

    Feat: Created Component to CTA Buttons

---
+++ src/components/ButtonCTA/Button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ButtonCTA = () => {
+  return (
+    <>
+      <div className="flex items-center">
+        <a
+          href="https://wa.me/5588993368262?text=Ol%C3%A1%21+Vi+seu+n%C3%BAmero+pelo+site.+Gostaria+de+mais+informa%C3%A7%C3%B5es+sobre+os+servi%C3%A7os."
+          className="inline-flex items-center justify-center h-12 px-4  font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-blue-700 hover:bg-blue-800 focus:shadow-outline focus:outline-none"
+        >
+          Solicitar orçamento
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default ButtonCTA;
