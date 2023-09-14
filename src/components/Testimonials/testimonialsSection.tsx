3d725ad8a9816696b1269aa9b30fb72eac510ef1
João Alcântara <joaosantalcantara@gmail.com>
2023-09-14 19:39:51 -0300

    Feat: Added new style & structure for Section

--- src/components/Testimonials/testimonialsSection.tsx
+++ src/components/Testimonials/testimonialsSection.tsx
@@ -9,302 +9,70 @@ import BG_03 from "../../../public/testimonials/gov_ceara.png";
 import BG_04 from "../../../public/testimonials/odontocenter.png";
 import BG_05 from "../../../public/testimonials/pref_russas.png";
 import BG_06 from "../../../public/testimonials/recautec.png";
-import BG_07 from "../../../public/testimonials/extrafarma.png";
-import BG_08 from "../../../public/testimonials/gov_ceara.png";
-import BG_09 from "../../../public/testimonials/odontocenter.png";
-import BG_10 from "../../../public/testimonials/pref_russas.png";
-import BG_11 from "../../../public/testimonials/recautec.png";
-import BG_12 from "../../../public/testimonials/extrafarma.png";
-import BG_13 from "../../../public/testimonials/gov_ceara.png";
-import BG_14 from "../../../public/testimonials/odontocenter.png";
-import BG_15 from "../../../public/testimonials/pref_russas.png";
-
-const Image_BG_01 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_01}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-const Image_BG_02 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_02}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-const Image_BG_03 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_03}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-const Image_BG_04 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_04}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-const Image_BG_05 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_05}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-const Image_BG_06 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_06}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-const Image_BG_07 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60 ">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_07}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-const Image_BG_08 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60 ">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_08}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-const Image_BG_09 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60 ">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_09}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-const Image_BG_10 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60 ">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_10}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-const Image_BG_11 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60 ">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_11}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-const Image_BG_12 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60 ">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_12}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-const Image_BG_13 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60 ">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_13}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-const Image_BG_14 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60 ">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_14}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-const Image_BG_15 = () => {
-  return (
-    <>
-      <div className="flex justify-center items-center relative h-32 lg:h-64 w-56 lg:w-60 ">
-        <Image
-          className="relative inset-0 h-auto w-32 lg:w-48 object-cover"
-          src={BG_15}
-          alt="bg_01"
-        />
-      </div>
-    </>
-  );
-};
-
-const IMAGES = [
-  <Image_BG_01 />,
-  <Image_BG_02 />,
-  <Image_BG_03 />,
-  <Image_BG_04 />,
-  <Image_BG_05 />,
-  <Image_BG_06 />,
-  <Image_BG_07 />,
-  <Image_BG_08 />,
-  <Image_BG_09 />,
-  <Image_BG_10 />,
-  <Image_BG_11 />,
-  <Image_BG_12 />,
-  <Image_BG_13 />,
-  <Image_BG_14 />,
-  <Image_BG_15 />,
-];
-
-export const InfiniteSlider = () => {
-  return (
-    <>
-      <div className="w-full w-max-screen">
-        <div className="relative mx-auto w-11/12 overflow-hidden bg-white before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] after:content-['']">
-          <div className="animate-infinite-slider flex w-[calc(250px*10)]">
-            {IMAGES.map((logo, index) => (
-              <div
-                className="slide flex items-center justify-center"
-                key={index}
-              >
-                {logo}
-              </div>
-            ))}
-            {IMAGES.map((logo, index) => (
-              <div
-                className="slide flex w-full items-center justify-center text-black"
-                key={index}
-              >
-                {logo}
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-export const InfiniteSlider2 = () => {
-  return (
-    <>
-      <div className="w-full w-max-screen">
-        <div className="relative mx-auto w-11/12 overflow-hidden bg-white before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] after:content-['']">
-          <div className="animate-infinite-slider2 flex w-[calc(250px*10)]">
-            {IMAGES.map((logo, index) => (
-              <div
-                className="slide flex items-center justify-center"
-                key={index}
-              >
-                {logo}
-              </div>
-            ))}
-            {IMAGES.map((logo, index) => (
-              <div
-                className="slide flex w-full items-center justify-center text-black"
-                key={index}
-              >
-                {logo}
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
 
 const TestimonialsSection = () => {
   return (
     <>
-      <div id="clientes">
-        <div className="container mx-auto">
-          <div className="flex relative flex-row w-full w-max-full">
-            <InfiniteSlider />
+      <section className="pt-20 pb-16 bg-white" id="clientes">
+        <div className="flex justity-center items-center py-auto my-auto container mx-auto px-5 lg:px-32">
+          <div className="w-full -m-1 flex flex-wrap md:-m-2">
+            <div className="flex w-1/3 h-full flex-wrap justify-center items-center my-auto">
+              <div className="w-2/3 h-full p-1 md:p-2">
+                <Image
+                  alt="gallery"
+                  className="block h-full w-full rounded-lg object-cover object-center"
+                  src={BG_01}
+                />
+              </div>
+            </div>
+            <div className="flex w-1/3 h-full flex-wrap justify-center items-center my-auto">
+              <div className="w-2/3 h-full p-1 md:p-2">
+                <Image
+                  alt="gallery"
+                  className="block h-full w-full rounded-lg object-cover object-center"
+                  src={BG_02}
+                />
+              </div>
+            </div>
+            <div className="flex w-1/3 h-full flex-wrap justify-center items-center my-auto">
+              <div className="w-2/3 h-full p-1 md:p-2">
+                <Image
+                  alt="gallery"
+                  className="block h-full w-full rounded-lg object-cover object-center"
+                  src={BG_03}
+                />
+              </div>
+            </div>
+            <div className="flex w-1/3 h-full flex-wrap justify-center items-center my-auto">
+              <div className="w-2/3 h-full p-1 md:p-2">
+                <Image
+                  alt="gallery"
+                  className="block h-full w-full rounded-lg object-cover object-center"
+                  src={BG_04}
+                />
+              </div>
+            </div>
+            <div className="flex w-1/3 h-full flex-wrap justify-center items-center my-auto">
+              <div className="w-2/3 h-full p-1 md:p-2">
+                <Image
+                  alt="gallery"
+                  className="block h-full w-full rounded-lg object-cover object-center"
+                  src={BG_05}
+                />
+              </div>
+            </div>
+            <div className="flex w-1/3 h-full flex-wrap justify-center items-center my-auto">
+              <div className="w-2/3 h-full p-1 md:p-2">
+                <Image
+                  alt="gallery"
+                  className="block h-full w-full rounded-lg object-cover object-center"
+                  src={BG_06}
+                />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="container mx-auto">
-          <div className="flex relative flex-row w-full w-max-full">
-            <InfiniteSlider2 />
-          </div>
-        </div>
-      </div>
+      </section>
     </>
   );
 };
