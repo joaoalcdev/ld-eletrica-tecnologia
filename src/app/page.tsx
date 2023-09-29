5ce05f033cc968f96c1bc215f9af0e4f52f8a921
João Alcântara <joaosantalcantara@gmail.com>
2023-09-29 16:39:53 -0300

    Feat: Added new Button CTA to Whatsapp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import DividerComponent from "@/components/Dividers/dividerComponent";
 import TestimonialsSection from "@/components/Testimonials/testimonialsSection";
 import Footer from "@/components/Footer/footer";
 import CTASection from "@/components/CTASection/ctaSection";
+import WhatsappCTA from "@/components/WhatsappButton/WhatsappCTA";
 
 export default function Home() {
   return (
     <>
+      <WhatsappCTA></WhatsappCTA>
       <HeroSection></HeroSection>
       <DividerComponent></DividerComponent>
       <AdvantageSection></AdvantageSection>
