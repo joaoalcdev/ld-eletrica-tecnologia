dd45cad2f671a41d1d5d2bad7a96f1a337dcb193
João Alcântara <joao.alcantara@avess.software>
2023-11-17 11:10:15 -0300

    feat: Created files & Added configuration to Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
 import Head from "next/head";
+import TagManager from "./service/tagmanager";
 
 const poppins = Poppins({
   weight: ["400"],
@@ -21,6 +23,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+}              )(window,document,'script','dataLayer','GTM-NKFPLQ5R');`,
+          }}
+        ></script>
+        <TagManager id="GTM-NKFPLQ5R" />
         <link
           rel="icon"
           href="./favicon.ico"
@@ -29,7 +42,18 @@ export default function RootLayout({
           sizes="32x32"
         />
       </Head>
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NKFPLQ5R"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        <TagManager id="GTM-NKFPLQ5R" />
+        {children}
+      </body>
     </html>
   );
 }
